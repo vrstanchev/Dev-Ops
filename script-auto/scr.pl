14e677225ab89ace3b1e25c72d4fb128f1706e60
vrstanchev <vrstanchev@gmail.com>
2024-02-03 10:51:58 +0200

    script-auto-project-preparation-done

---
+++ script-auto/scr.pl
@@ -0,0 +1,7 @@
+!# /bin/perl
+my $iter=3;
+my @array=("fst", "snd","trd");
+for($i=0;i<=$iter;$i++){
+print(@array[i]);
+}
+print("Done");
---
+++ script-auto/scr.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+print("begin")
+chmod +x scr.pl
+perl scr.pl
+print("end")
